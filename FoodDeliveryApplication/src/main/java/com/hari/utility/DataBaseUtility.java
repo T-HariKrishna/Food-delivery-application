240735eef9095982d3cf2a4f70886dd6a2481084
Hari Krishna <krishnatalari149@gmail.com>
2026-02-03 17:35:34 +0530

    Prepare application for cloud deployment: update DB utility for env variables and fix servlet jakarta.servlet-api 6.0.0 to 5.0.0 version in POM
    Merge branch 'master' of
    https://github.com/T-HariKrishna/Food-delivery-application

--- FoodDeliveryApplication/src/main/java/com/hari/utility/DataBaseUtility.java
+++ FoodDeliveryApplication/src/main/java/com/hari/utility/DataBaseUtility.java
@@ -7,21 +7,28 @@ import java.sql.SQLException;
 public class DataBaseUtility {
 	
 	static Connection connection;
-	private static final String URL="jdbc:mysql://localhost:3306/food_delivery_project";
-	private static final String USERNAME="root";
-	private static final String PASSWORD="root@123";
 	
 	
 	public static Connection getConnection() {
 
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+		 try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
 
-			System.out.println("Driver found");
-			connection=DriverManager.getConnection(URL,USERNAME,PASSWORD);
-		} catch (ClassNotFoundException  |SQLException e) {
-			e.printStackTrace();
-		}
+	            // Read from Environment Variables
+	            String url = System.getenv("DB_URL");
+	            String username = System.getenv("DB_USER");
+	            String password = System.getenv("DB_PASSWORD");
+
+	            if (url == null || username == null || password == null) {
+	                throw new RuntimeException("Database environment variables not set");
+	            }
+
+	            connection = DriverManager.getConnection(url, username, password);
+	            System.out.println("Database Connected Successfully");
+
+	        } catch (ClassNotFoundException | SQLException e) {
+	            e.printStackTrace();
+	        }
 		
 		return connection;
 	}
